aaa52565f586ed511d4f13b79dc61ccbd57a1258
unluque <adrianluque211@gmail.com>
2020-02-18 14:07:03 +0100

    Direccion

--- src/main/java/Direcciones/Direccion.java
+++ src/main/java/Direcciones/Direccion.java
@@ -1,4 +1,26 @@
 package Direcciones;
 
 public class Direccion {
+
+    String codigo_postal;
+    String provincia;
+    String poblacion;
+
+    public Direccion(String codigo_postal, String provincia, String poblacion) {
+        this.codigo_postal = codigo_postal;
+        this.provincia = provincia;
+        this.poblacion = poblacion;
+    }
+
+    public String getCodigo_postal() {
+        return codigo_postal;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public String getPoblacion() {
+        return poblacion;
+    }
 }
