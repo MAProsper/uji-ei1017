fff497a74f5a9307b3df280a7070b118eea9bf3f
Miguel Angel Prosper <37023010+MAProsper@users.noreply.github.com>
2020-02-18 14:08:44 +0100

    Clientes getters

--- src/main/java/Clientes/Cliente.java
+++ src/main/java/Clientes/Cliente.java
@@ -1,4 +1,45 @@
 package Clientes;
 
+import Direcciones.Direccion;
+import Tarifas.Tarifa;
+
+import java.util.Date;
+
 public class Cliente {
+    String nombre;
+    String NIF;
+    Direccion direccion;
+    String correo;
+    Date fechaAlta;
+    Tarifa tarifa;
+
+    public Cliente(String nombre, String NIF, Date fechaAlta) {
+        this.nombre = nombre;
+        this.NIF = NIF;
+        this.fechaAlta = fechaAlta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNIF() {
+        return NIF;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public Tarifa getTarifa() {
+        return tarifa;
+    }
 }
--- src/main/java/Clientes/ClientePaticular.java
+++ src/main/java/Clientes/ClientePaticular.java
@@ -1,4 +1,12 @@
 package Clientes;
 
-public class ClientePaticular {
+import java.util.Date;
+
+public class ClientePaticular extends Cliente {
+    String apellidos;
+
+    public ClientePaticular(String nombre, String apellidos, String NIF, Date fechaAlta) {
+        super(nombre, NIF, fechaAlta);
+        this.apellidos = apellidos;
+    }
 }
