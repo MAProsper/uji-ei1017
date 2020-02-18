eb706dfe9cc570d758631f7c56afb5fb24b7bd4a
Miguel Angel Prosper <37023010+MAProsper@users.noreply.github.com>
2020-02-18 14:22:39 +0100

    Llamada implemented

--- src/main/java/Llamadas/Llamada.java
+++ src/main/java/Llamadas/Llamada.java
@@ -1,4 +1,27 @@
 package Llamadas;
 
+import java.util.Date;
+
 public class Llamada {
+    String telefono;
+    Date fecha;
+    float duracion;
+
+    public Llamada(String telefono, Date fecha, float duracion) {
+        this.telefono = telefono;
+        this.fecha = fecha;
+        this.duracion = duracion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public float getDuracion() {
+        return duracion;
+    }
 }
