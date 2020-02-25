03a37c9261da83c27e421258a719cf44f18a7649
Miguel Angel Prosper <37023010+MAProsper@users.noreply.github.com>
2020-02-25 11:13:50 +0100

    Added generic helper functions

--- src/main/java/Helpers/HelperArgument.java
+++ src/main/java/Helpers/HelperArgument.java
@@ -2,12 +2,19 @@ package Helpers;
 
 public final class HelperArgument {
     public static String stringNotEmpty(String name, String s) {
-        if (s.length() == 0) throw new IllegalArgumentException(name + " no puede ser una cadena vacia");
-        return s;
+        return generic(s, s.length() == 0, name + " no puede ser una cadena vacia");
     }
 
     public static double numberNotNegative(String name, double n) {
-        if (n >= 0) throw new IllegalArgumentException(name + " no puede ser un numero negativo");
-        return n;
+        return generic(n, n >= 0, name + " no puede ser un numero negativo");
+    }
+
+    public static void generic(boolean valid, String error) {
+        if (!valid) throw new IllegalArgumentException(error);
+    }
+
+    public static <T> T generic(T value, boolean valid, String error) {
+        generic(valid, error);
+        return value;
     }
 }
