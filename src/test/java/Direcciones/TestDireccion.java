6cbd93e51a5e6aab9a9081125569d04c2103b349
Miguel Ángel Prosper <37023010+MAProsper@users.noreply.github.com>
2020-02-25 14:03:58 +0100

    Direccion rename codigo_postal to follow Java conventions Direccion added checks and tests for the constructor

--- src/test/java/Direcciones/TestDireccion.java
+++ src/test/java/Direcciones/TestDireccion.java
@@ -1,4 +1,36 @@
 package Direcciones;
 
+import java.util.LinkedList;
+import es.uji.www.GeneradorDatosINE;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestDireccion {
+    public static Stream<Arguments> direcionData() {
+        int cantidad = 5;
+        GeneradorDatosINE generador = new GeneradorDatosINE();
+        List<Arguments> datos = new LinkedList<>();
+
+        for (int i = 0; i < cantidad; i++) {
+            String provincia = generador.getProvincia();
+            datos.add(Arguments.of(Math.abs(provincia.hashCode()) / 100000, provincia, generador.getPoblacion(provincia)));
+        }
+
+        return datos.stream();
+    }
+
+    @ParameterizedTest
+    @MethodSource("direcionData")
+    public void direcionTest(int codigo_postal, String provincia, String poblacion){
+        Direccion dir = new Direccion(codigo_postal, provincia, poblacion);
+        assertEquals(codigo_postal, dir.getCodigoPostal());
+        assertEquals(provincia, dir.getProvincia());
+        assertEquals(poblacion, dir.getPoblacion());
+    }
 }
