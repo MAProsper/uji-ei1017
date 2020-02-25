2fd2a0cdb454825df5c688e394fab715e8589c67
Miguel Angel Prosper <37023010+MAProsper@users.noreply.github.com>
2020-02-25 11:18:13 +0100

    Fix generics and added referenceNotNull

--- src/main/java/Helpers/HelperArgument.java
+++ src/main/java/Helpers/HelperArgument.java
@@ -1,14 +1,6 @@
 package Helpers;
 
 public final class HelperArgument {
-    public static String stringNotEmpty(String name, String s) {
-        return generic(s, s.length() == 0, name + " no puede ser una cadena vacia");
-    }
-
-    public static double numberNotNegative(String name, double n) {
-        return generic(n, n >= 0, name + " no puede ser un numero negativo");
-    }
-
     public static void generic(boolean valid, String error) {
         if (!valid) throw new IllegalArgumentException(error);
     }
@@ -16,5 +8,17 @@ public final class HelperArgument {
     public static <T> T generic(T value, boolean valid, String error) {
         generic(valid, error);
         return value;
+    }
+
+    public static String stringNotEmpty(String name, String s) {
+        return generic(s, s.length() != 0, name + " no puede ser una cadena vacia");
+    }
+
+    public static double numberNotNegative(String name, double n) {
+        return generic(n, n < 0, name + " no puede ser un numero negativo");
+    }
+
+    public static <T> T referenceNotNull(String name, T value) {
+        return generic(value, value != null, name + " no puede ser nulo");
     }
 }
