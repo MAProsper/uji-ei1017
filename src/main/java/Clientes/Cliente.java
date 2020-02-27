8e9b522133c493b3a9dd1612edb96f238979aabf
Miguel Ángel Prosper <37023010+MAProsper@users.noreply.github.com>
2020-02-27 21:02:52 +0100

    Added extra restrictions to Cliente

--- src/main/java/Clientes/Cliente.java
+++ src/main/java/Clientes/Cliente.java
@@ -2,6 +2,8 @@ package Clientes;
 
 import java.util.*;
 
+import Facturas.Factura;
+import Llamadas.Llamada;
 import Tarifas.Tarifa;
 import Direcciones.Direccion;
 
@@ -14,60 +16,75 @@ public class Cliente {
     final String correo;
     final Date fechaAlta;
     Tarifa tarifa;
+    final List<Llamada> llamadas;
+    final List<Factura> facturas;
 
     public Cliente(final String NIF, final String nombre, final Direccion direccion, final String correo, final Date fechaAlta, final Tarifa tarifa) {
-        this.nombre = stringNotEmpty("nombre", nombre);
         this.NIF = stringMatchesPattern("NIF", NIF, "\\d+[TRWAGMYFPDXBNJZSQVHLCKE]");
+        this.nombre = stringNotEmpty("nombre", nombre);
         this.direccion = referenceNotNull("direccion", direccion);
         this.correo = stringMatchesPattern("correo", correo, "[\\w.]+@[\\w.]{2,}");
         this.fechaAlta = referenceNotNull("fechaAlta", fechaAlta);
-        this.tarifa = referenceNotNull("tarifa", tarifa);
+        setTarifa(tarifa);
+        llamadas = new LinkedList<>();
+        facturas = new LinkedList<>();
     }
 
-    public String getNombre() {
+    final public String getNombre() {
         return nombre;
     }
 
-    public String getNIF() {
+    final public String getNIF() {
         return NIF;
     }
 
-    public Direccion getDireccion() {
+    final public Direccion getDireccion() {
         return direccion;
     }
 
-    public String getCorreo() {
+    final public String getCorreo() {
         return correo;
     }
 
-    public Date getFecha() {
+    final public Date getFecha() {
         return fechaAlta;
     }
 
-    public Tarifa getTarifa() {
+    final public Tarifa getTarifa() {
         return tarifa;
     }
 
     public void setTarifa(final Tarifa tarifa) {
-        this.tarifa = tarifa;
+        this.tarifa = referenceNotNull("tarifa", tarifa);
+    }
+
+    final public List<Llamada> getLlamadas() {
+        return Collections.unmodifiableList(llamadas);
+    }
+
+    public void addLlamada(Llamada llamada) {
+        llamadas.add(llamada);
+    }
+
+    final public List<Factura> getFacturas() {
+        return Collections.unmodifiableList(facturas);
+    }
+
+    public void addFactura(Factura factura) {
+        facturas.add(factura);
     }
 
     @Override
-    public boolean equals(final Object o) {
+    final public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        final Cliente cliente = (Cliente) o;
-        return Objects.equals(nombre, cliente.nombre) &&
-                Objects.equals(NIF, cliente.NIF) &&
-                Objects.equals(direccion, cliente.direccion) &&
-                Objects.equals(correo, cliente.correo) &&
-                Objects.equals(fechaAlta, cliente.fechaAlta) &&
-                Objects.equals(tarifa, cliente.tarifa);
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(NIF, cliente.NIF);
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(nombre, NIF, direccion, correo, fechaAlta, tarifa);
+    final public int hashCode() {
+        return Objects.hash(NIF);
     }
 
     @Override
--- src/main/java/Clientes/ClientePaticular.java
+++ src/main/java/Clientes/ClientePaticular.java
@@ -15,22 +15,8 @@ public class ClientePaticular extends Cliente {
         this.apellidos = stringNotEmpty("apellidos", apellidos);
     }
 
-    public String getApellidos() {
+    final public String getApellidos() {
         return apellidos;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        final ClientePaticular that = (ClientePaticular) o;
-        return Objects.equals(apellidos, that.apellidos);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), apellidos);
     }
 
     @Override
