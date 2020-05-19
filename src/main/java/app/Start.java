98668abb26be3a235b4a265a0bddc711583594e3
Miguel Ángel Prosper <37023010+MAProsper@users.noreply.github.com>
2020-05-19 14:45:36 +0200

    Cleanup start

--- src/main/java/app/Start.java
+++ src/main/java/app/Start.java
@@ -1,14 +1,10 @@
 package app;
 
-import java.nio.file.Paths;
-
 public class Start {
     private Start() {
     }
 
     public static void main(String[] args) {
-        Gestor gestor = new Gestor();
-        gestor.load(Paths.get("target/clientes4.bin"));
-        gestor.show();
+        new Gestor().show();
     }
 }
